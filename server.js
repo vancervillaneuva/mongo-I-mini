debc0ed025c9fc4ce1adb854879577bb9d095c22
Vance Villanueva <vancervillanueva@gmail.com>
2017-08-28 20:52:26 -0700

    pushing mongo

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+const Bear = require('./models');
 
 const STATUS_USER_ERROR = 422;
 const STATUS_SERVER_ERROR = 500;
@@ -9,9 +10,52 @@ const server = express();
 // allow server to parse JSON bodies from POST/PUT/DELETE requests
 server.use(bodyParser.json());
 
+server.post('/bears', (req,res) => {
+	
+	const {species, latinName} = req.body; 
+
+     if (!species || !latinName) {
+        res.status(422);
+        res.json('missing text or complieted field');
+     return;
+     }
+
+     //const bear = new Bear({species, latinName});
+     const bear = new Bear(req.body);
+
+    //  bearTypes.save((err) => {
+    //      if (err) throw err;
+    //      res.json(todo);
+    //  });
+   bear.save((err, savedBear)=>{
+     if (err) {
+       res.status(500).json({ success: false, message: 'could not save the bear'});
+     } else {
+       res.status(201).json(savedBear);
+     }
+   });
+
+});
+
+server.get('/bears', (req, res) => {
+	Bear.find({}, (err, data) => {
+        if(err) throw err;
+        res.json(data);
+  });
+      
+// ext Bear.find({/bears})
+
+});
 
 
+server.get('/bears/:id', (req, res) => {
+	const { id } = req.params; //? query string
 
+	Bear.findById(id, (err, bear) => {
+        if (err) throw err;
+          res.json(bear);
+	});
+});
 
 
 // TODO: write your server code here
